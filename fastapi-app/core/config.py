8c2c7e654e7062dbb4a4a783c71185ad1db07e08
Egor_Yakovlev1 <egor.yakovlev.95@gmail.com>
2024-10-07 00:42:27 +0300

    create db config and db helper

--- fastapi-app/core/config.py
+++ fastapi-app/core/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -7,12 +8,20 @@ class RunConfig(BaseModel):
     port: int = 8000
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     # run_configuration
     run: RunConfig = RunConfig()
 
     # db
-    # db_url: str
+    db: DatabaseConfig
 
     # prefix
     api_prefix: str = "/api"
