01c2f780619b5b6a9f1be57fb75c3eb9b2e9be91
Egor_Yakovlev1 <egor.yakovlev.95@gmail.com>
2024-11-03 23:08:58 +0300

    add http_bearer for auth token

--- fastapi-app/api/api_v1/__init__.py
+++ fastapi-app/api/api_v1/__init__.py
@@ -1,12 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
+
 from core.config import settings
 from .auth import router as auth_router
 from .users import router as users_router
 
 
+# что бы входить через токен
+http_bearer = HTTPBearer(auto_error=False)
+
 # Подключение роутера(ов)
 router = APIRouter(
-    prefix=settings.api.v1.prefix
+    prefix=settings.api.v1.prefix,
+    dependencies=[Depends(http_bearer)]
 )
 
 router.include_router(auth_router)
