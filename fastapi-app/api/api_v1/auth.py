e35ab855ae3a0bd3c193895b6ccb697f8ca263bc
Egor_Yakovlev1 <egor.yakovlev.95@gmail.com>
2024-11-04 00:11:49 +0300

    create verifications routers

--- fastapi-app/api/api_v1/auth.py
+++ fastapi-app/api/api_v1/auth.py
@@ -7,15 +7,31 @@ from core.schemas.user import UserRead, UserCreate
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
-    tags=["Auth"]
+    tags=["Auth"],
 )
 
 # /login /logout
 router.include_router(
-    router=fastapi_users.get_auth_router(authentication_backend),
+    router=fastapi_users.get_auth_router(
+        authentication_backend,
+        # requires_verification=True,
+    ),
 )
 
 # /register
 router.include_router(
-    router=fastapi_users.get_register_router(UserRead, UserCreate),
+    router=fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
+)
+
+# /register-verify-token /verify
+router.include_router(
+    router=fastapi_users.get_verify_router(UserRead),
+)
+
+# /forgot-password /reset-password
+router.include_router(
+    router=fastapi_users.get_reset_password_router(),
 )
